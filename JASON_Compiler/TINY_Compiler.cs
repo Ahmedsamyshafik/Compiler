ea2e40daac1ca20a2fe2d3e7d668892426670c67
Ahmed Samy <ah6med.saaammmy515@gmail.com>
2024-04-20 03:08:53 +0200

    Add project files.

---
+++ JASON_Compiler/TINY_Compiler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TINY_Compiler
+{
+    public static class TINY_Compiler
+    {
+        public static Scanner Tiny_Scanner = new Scanner();
+        public static Parser Tiny_Parser = new Parser();
+        public static List<Token> TokenStream = new List<Token>();
+        public static Node treeroot;
+
+        public static void Start_Compiling(string SourceCode) //character by character
+        {
+            Tiny_Scanner.StartScanning(SourceCode);
+            Tiny_Parser.StartParsing(TokenStream);
+            treeroot = Tiny_Parser.root;
+        } 
+    }
+}
